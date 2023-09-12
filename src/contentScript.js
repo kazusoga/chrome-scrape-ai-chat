ce9caa9ff97d056d14d217f257076faa5c17174e
曽我 和久 <kazusoga1002guitar.42p67@gmail.com>
2023-09-12 13:51:02 +0900

    fix: テキスト選択時にボタンを生成する

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,43 +1,29 @@
-'use strict';
+(function() {
+  // Keep track of the button element and selected text
+  let button;
 
-// Content script file will run in the context of web page.
-// With content script you can manipulate the web pages using
-// Document Object Model (DOM).
-// You can also pass information to the parent extension.
+  document.addEventListener('mouseup', function(event) {
+    // button のクリックイベントの場合は何もしない
+    if (event.target === button) {
+      return;
+    }
+    // button 以外の箇所のクリックで、button が存在する場合は削除する
+    if (event.target !== button && button) {
+      button.remove();
+      button = null;
+      return;
+    }
+    // Get the selected text
+    const selectedText = window.getSelection().toString().trim();
 
-// We execute this script by making an entry in manifest.json file
-// under `content_scripts` property
+    if (selectedText.length > 0) {
+      button = document.createElement('button');
+      button.innerHTML = selectedText;
+      button.style.position = 'absolute';
+      button.style.left = event.pageX + 'px';
+      button.style.top = event.pageY + 'px';
 
-// For more information on Content Scripts,
-// See https://developer.chrome.com/extensions/content_scripts
-
-// Log `title` of current active web page
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
-
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  (response) => {
-    console.log(response.message);
-  }
-);
-
-// Listen for message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
-  }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
-  return true;
-});
+      document.body.appendChild(button);
+    }
+  });
+})();
